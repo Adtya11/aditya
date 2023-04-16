f81fd20f654660985fd937d861132ffad1742d6c
Adtya11 <aditya.srivastava@cvent.com>
2023-04-17 02:51:24 +0530

    Update pnpm lock file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head key={null}></Head>
+      <Head key={null}> </Head>
       <div className="body">
         <NavBar />
         <Component {...pageProps} />
